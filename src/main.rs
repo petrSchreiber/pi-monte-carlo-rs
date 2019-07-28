b880a1cdf35c9a8f5b4784e572c76b569c07acbb
petrSchreiber <petrschreiber@gmail.com>
2019-07-28 10:59:39 +0200

    feat: adding tests and discovering mistake

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ struct Point2D {
     pub y: f64,
 }
 
-fn is_inside_unit_circle(point: &Point2D) -> bool {
-    (point.x * point.x + point.y * point.y).sqrt() < 1.0
+fn belongs_unit_circle(point: &Point2D) -> bool {
+    (point.x * point.x + point.y * point.y).sqrt() <= 1.0
 }
 
 fn approximate_pi(max_point: u64) -> f64 {
@@ -21,7 +21,7 @@ fn approximate_pi(max_point: u64) -> f64 {
             y: rng.gen_range(0.0, 1.0),
         };
 
-        if is_inside_unit_circle(&point) {
+        if belongs_unit_circle(&point) {
             points_in_circle += 1;
         }
     }
@@ -29,7 +29,7 @@ fn approximate_pi(max_point: u64) -> f64 {
         "{} points in circle out of {} total points.",
         points_in_circle, max_point
     );
-    
+
     4.0f64 * f64::from(points_in_circle) / max_point as f64
 }
 
@@ -52,4 +52,31 @@ fn main() {
     };
 
     println!("{}", approximate_pi(max_points));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn belongs_unit_circle_for_inside_point() {
+        let point_inside = Point2D { x: 0.0, y: 0.0 };
+
+        assert!(belongs_unit_circle(&point_inside) == true);
+    }
+
+    #[test]
+    fn belongs_unit_circle_for_outside_point() {
+        let point_outside = Point2D { x: 2.0, y: 0.0 };
+
+        assert!(belongs_unit_circle(&point_outside) == false);
+    }
+
+    #[test]
+    fn belongs_unit_circle_for_edge_point() {
+        let point_on_the_edge = Point2D { x: 1.0, y: 0.0 };
+
+        assert!(belongs_unit_circle(&point_on_the_edge) == true);
+    }
+
 }
